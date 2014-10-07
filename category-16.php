de364716a2c62f727b1e8422eda37bc30c655d29
Silvia Mantovani <silphoenix@users.noreply.github.com>
2014-10-07 10:59:34 +0200

    Update category-16.php

--- category-16.php
+++ category-16.php
@@ -1,40 +1,27 @@
 <?php get_header(); ?>
 
+<br /> <br/>
 <ul class="gallerylist">
 				
- <?php if (have_posts()) :?><?php while(have_posts()) : the_post(); ?> 
- <li class="owl-demo" >    
-
-   <div style="overflow:hidden; margin-bottom:100px; max-height:200px; "  class="js-masonry"
-  data-masonry-options='{ "columnWidth": 1, "itemSelector": ".item" }' >
-			
-		<!--<?php //$image_ids = get_field('galleria', false, false);
-				//$shortcode = '[gallery ids="' . implode(',', $image_ids) . '"]';
-					//if($image_ids):
-					//foreach( $image_ids as $image ): 
-					//$conta++; ?>
-		
-				<?php //endforeach;
-					//endif;		 ?>
-				  <?php //echo do_shortcode( $shortcode ); ?>-->
-
+<?php if (have_posts()) :?><?php while(have_posts()) : the_post(); ?> 
+ <li >    
+<div >
 	<?php $images = get_field('galleria');
 
 		if( $images ): ?>
-		<ul >
+		<ul class="owl-demo" class="js-masonry" data-masonry-options='{ "columnWidth": 60, "itemSelector": ".masonry"}'>
 			<?php foreach( $images as $image ):
 				$conta++; ?>			
-            <li>
-                <a href="<?php echo $image['url']; ?>">
-                     <img class="item" src="<?php echo $image['sizes']['thumbnail'];?>"   />
+            <li class="item masonry"  >
+                <a href="<?php echo $image['url']; ?>" >
+                    <img src="<?php echo $image['sizes']['thumbnail'];?>"/>
                 </a>
             </li>
 			<?php endforeach; ?>
 		</ul>
 			<?php endif; ?>
+	</div>
 			
-    </div> <!-- .entry-content -->
-		
 	<div class="customNavigation">
 		<a class="btn prev" style="float:left; position:relative; z-index:4; margin-top: -200px;"><img src="http://narayana.tlco.info/wp-content/uploads/2014/09/frecciasxbianca1.png"/></a>
 		<a class="btn next" style="float:right; position:relative; z-index:4; margin-top: -200px;"><img src="http://narayana.tlco.info/wp-content/uploads/2014/09/frecciadxbianca1.png"/></a>
@@ -49,9 +36,9 @@
 			<p style="margin-left:-35px; font-size:40px; font-weight:400; margin-top: -5px;"><?php echo $conta;?></p>
 			<p style="font-size:25px; margin-left:-35px; margin-top:-30px;">FOTO</p>
 		</div>
-	</div>					
-			
+	</div>							
  </li>        
+ 
  <?php 
  $conta=0;
  endwhile; ?>               
@@ -68,33 +55,24 @@
  <script>
 $(document).ready(function() {
 	
-	$(".next").click(function () {
+ var owl = $(".owl-demo");
  
-    // Set the effect type
-    var effect = 'slide';
- 
-    // Set the options for the effect type chosen
-    var options = { direction: 'left' };
- 
-    // Set the duration (default: 400 milliseconds)
-    var duration = 700;
- 
-    $('.item').toggle(effect, options, duration);
+owl.owlCarousel({
+items : 4, //10 items above 1000px browser width
+itemsDesktop : [1000,4], //5 items between 1000px and 901px
+itemsDesktopSmall : [900,3], // betweem 900px and 601px
+itemsTablet: [600,2], //2 items between 600 and 0
+itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
 });
+ 
+// Custom Navigation Events
+$(".next").click(function(){
+owl.trigger('owl.next');
+})
+$(".prev").click(function(){
+owl.trigger('owl.prev');
+})
 
-	$(".prev").click(function () {
- 
-    // Set the effect type
-    var effect = 'slide';
- 
-    // Set the options for the effect type chosen
-    var options = { direction: 'right' };
- 
-    // Set the duration (default: 400 milliseconds)
-    var duration = 700;
- 
-    $('.item').toggle(effect, options, duration);
-});
 
 
 });
